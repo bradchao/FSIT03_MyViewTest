34218a48ced06870f6d0dd2807b53d09e9424e7b
Chien <chien@gmail.com>
2017-08-12 14:05:10 +0800

    v2

---
+++ app/src/main/java/tw/brad/app/helloworld/myviewtest/MyView.java
@@ -0,0 +1,14 @@
+package tw.brad.app.helloworld.myviewtest;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MyView extends View {
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        setBackgroundColor(Color.GREEN);
+    }
+}
